91965d06982838ded15c903cc11825fbbb45142c
huy.tran <huy.tran@altsourcesoftware.com>
2020-06-08 18:40:22 +0700

    add http output

---
+++ ELK.Demo/Controllers/LogStashController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace ELK.Demo.Controllers
+{
+	[ApiController]
+	[Route("api/[controller]")]
+	public class LogStashController : ControllerBase
+	{
+
+		private readonly ILogger<LogStashController> _logger;
+
+		public LogStashController(ILogger<LogStashController> logger)
+		{
+			_logger = logger;
+		}
+		[HttpPost]
+		public IActionResult Index([FromBody] Object data)
+		{
+			_logger.LogInformation(data.ToString());
+			return Ok();
+		}
+	}
+}
